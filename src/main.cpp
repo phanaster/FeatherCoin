8e22454c65866806eea0202aa7ee777828293344
Feather Coin <feather@feathercoin.com>
2013-05-11 09:36:24 +0100

    Set difficulty adjust to 504 block

--- src/main.cpp
+++ src/main.cpp
@@ -835,7 +835,7 @@ int64 static GetBlockValue(int nHeight, int64 nFees)
     return nSubsidy + nFees;
 }
 
-static const int64 nTargetTimespan = 3.5 * 24 * 60 * 60; // Feathercoin: 3.5 days
+static const int64 nTargetTimespan = 0.875 * 24 * 60 * 60; // Feathercoin: 3.5 days
 static const int64 nTargetSpacing = 2.5 * 60; // Feathercoin: 2.5 minutes
 static const int64 nInterval = nTargetTimespan / nTargetSpacing;
 
